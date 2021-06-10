78d0497eb77154c61da1b55922da08da53c25fb0
001ZQG744 <001ZQG744@192.168.0.3>
2021-06-10 12:00:05 +0530

    array enum added constructor

--- arraystatic/src/comm/example/EmployeeType.java
+++ arraystatic/src/comm/example/EmployeeType.java
@@ -1,6 +1,17 @@
 package comm.example;
 
 public enum EmployeeType {
-	SALARIED,
-	HOORLY_PAID
+	SALARIED("Salaried Employee"),
+	HOORLY_PAID("Hourly Paid Employee");
+	
+	private String message;
+	
+	private EmployeeType(String message)
+	{
+		this.message=message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
 }
