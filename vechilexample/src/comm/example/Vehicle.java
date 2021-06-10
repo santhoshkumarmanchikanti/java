e926843ba08394e3ca67885f6510c172c8f48b45
001ZQG744 <001ZQG744@192.168.0.3>
2021-06-10 12:50:51 +0530

    my first vehicle examp

---
+++ vechilexample/src/comm/example/Vehicle.java
@@ -0,0 +1,18 @@
+package comm.example;
+
+public abstract class Vehicle {
+	public abstract double calcFuelEfficiency(double fuelConsumed);
+	public abstract double calcTripDistance(double startPoint,double endPoint);
+	
+	private String message;
+	public String getDetails()
+	{
+		return "hello";
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
