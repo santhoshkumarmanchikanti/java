02e43defcd425c07bfb61a208292311fdf6d01db
001ZQG744 <001ZQG744@192.168.0.3>
2021-06-11 11:08:23 +0530

    my snmr assign

---
+++ snmrcollege/src/comm/example/Hosteller.java
@@ -0,0 +1,27 @@
+package comm.example;
+
+public class Hosteller extends Student{
+    private String hostelName;
+    private int roomNumber;
+    public Hosteller()
+    {
+        hostelName=" ";
+        roomNumber=0;
+    }
+    public void setHostelName(String x)
+    {
+        hostelName=x;
+    }
+    public void setRoomNumber(int y)
+    {
+        roomNumber=y;
+    }
+    public String getHostelName()
+    {
+        return hostelName;
+    }
+    public int getRoomNumber()
+    {
+        return roomNumber;
+    }
+}
