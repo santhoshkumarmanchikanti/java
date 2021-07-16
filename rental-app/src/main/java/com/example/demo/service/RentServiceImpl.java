b9311214e7b7501e8dc4b0dddf82d04fcc3144f7
santhoshkumarmanchikanti <85543543+santhoshkumarmanchikanti@users.noreply.github.com>
2021-07-16 17:00:40 +0530

    Add files via upload

---
+++ rental-app/src/main/java/com/example/demo/service/RentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.RentRepository;
+import com.example.demo.model.RentEntity;
+@Service
+public class RentServiceImpl implements RentService{
+	private RentRepository rentRepository;
+	
+	
+	public RentServiceImpl(RentRepository rentRepository) {
+		this.rentRepository = rentRepository;
+	}
+
+	@Override
+	public RentEntity createRent(RentEntity rentEntity) {
+		
+		return rentRepository.save(rentEntity);
+	}
+
+	@Override
+	public List<RentEntity> displayAllRent() {
+		return rentRepository.findAll();
+	}
+
+}
