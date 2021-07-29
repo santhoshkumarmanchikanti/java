28840dd1b5484bbad425d606093e7c5d40f2dced
santhoshkumarmanchikanti <85543543+santhoshkumarmanchikanti@users.noreply.github.com>
2021-07-29 15:26:28 +0530

    junit5 spring boot

---
+++ junit5-spring-boot-example/src/test/java/com/howtoprogram/unit5/ApplicationTests.java
@@ -0,0 +1,36 @@
+package com.howtoprogram.unit5;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testAbout() {
+		String message = this.restTemplate.getForObject("/about", String.class);
+		assertEquals("JUnit 5 and Spring Boot Example.", message);
+	}
+	@Test
+	public void testPerson()
+	{
+		Person person=new Person(1, "John");
+		Person tempPerson=this.restTemplate.getForObject("/person", Person.class);
+		assertEquals(person.getName(), tempPerson.getName());
+		
+	}
+
+}
