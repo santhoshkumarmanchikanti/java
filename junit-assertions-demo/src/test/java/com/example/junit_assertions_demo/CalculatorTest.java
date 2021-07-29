e4dcf16f6e74bed22ebcfa9896d4568de377c195
santhoshkumarmanchikanti <santhoshkumarmanchikanti@gmail.com>
2021-07-29 12:22:44 +0530

    my first JUnit test demo

---
+++ junit-assertions-demo/src/test/java/com/example/junit_assertions_demo/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.example.junit_assertions_demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+	private Calculator calculator = new Calculator();
+
+	@Test
+	
+	public void testAddition() {
+		assertEquals(6, calculator.addition(3, 3));
+	}
+
+}
